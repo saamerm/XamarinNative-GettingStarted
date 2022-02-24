96bcdbdf7ffe1942145f682e1f28b5cf28987087
Saamer Mansoor <i@saamer.me>
2022-02-24 11:13:48 -0500

    Blank iOS & Android apps

---
+++ GettingStarted/GettingStarted.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GettingStarted.Core
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("Saamer");
+        }
+    }
+}
---
+++ GettingStarted/GettingStarted.iOS/ViewController.cs
@@ -0,0 +1,27 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using GettingStarted.Core;
+
+namespace GettingStarted.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var c = new Class1();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
